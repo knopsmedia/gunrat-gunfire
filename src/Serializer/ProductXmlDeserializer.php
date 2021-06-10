8cac53b37907a1810c7cb960483c8c783766d57c
Ignace Knops <ignace.knops@gmail.com>
2021-06-10 18:18:00 +0200

    Initial commit

---
+++ src/Serializer/ProductXmlDeserializer.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Gunratbe\Gunfire\Serializer;
+
+class ProductXmlDeserializer
+{
+$END$
+}
---
+++ src/Service/GunfireService.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Gunratbe\Gunfire\Service;
+
+class GunfireService
+{
+$END$
+}
